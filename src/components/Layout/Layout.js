732fa4aad61bc349e674cfe24a96bc2e4c73ba5f
Richard Ball <webalation@gmail.com>
2020-09-24 09:16:47 -0400

    Started implementing burger builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from "react";
+import classes from "./Layout.module.css";
 
 const layout = (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </React.Fragment>
 );
 
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
