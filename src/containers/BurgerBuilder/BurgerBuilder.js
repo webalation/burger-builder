277c0b47f7a2e871aa6a1c8164ed48b5a5b501b7
Richard Ball <webalation@gmail.com>
2020-09-25 06:17:54 -0400

    Started the Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </React.Fragment>
     );
