baec19077aac03e48d321775f41d7bef091dcf8e
Richard Ball <webalation@gmail.com>
2020-09-29 07:37:58 -0400

    Added the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -25,6 +25,9 @@ const buildControls = (props) => {
           disabled={props.disabled[ctrl.type]}
         />
       ))}
+      <button className={classes.OrderButton} disabled={!props.purchasable}>
+        ORDER NOW
+      </button>
     </div>
   );
 };
