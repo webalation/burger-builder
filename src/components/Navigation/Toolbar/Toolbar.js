ff7e6707fa6518b47a268ee3d9783e7c4de96020
Richard Ball <webalation@gmail.com>
2020-10-02 06:09:27 -0400

    Responsive adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,9 @@ import classes from "./Toolbar.module.css";
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
