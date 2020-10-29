160f451aae64eba0142750d6a85aa9344a56313b
Richard Ball <webalation@gmail.com>
2020-10-29 05:57:50 -0400

    Getting a Token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -23,5 +25,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBdliVACIThzKNCgRpCsyUSsnkaUVbJ49M",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail());
+      });
   };
 };
