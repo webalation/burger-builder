c59dff41a192c340ebe5a7b158ab9d527de56585
Richard Ball <webalation@gmail.com>
2020-11-10 07:29:41 -0500

    Moving the Orders Side Effects into Sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,7 +5,17 @@ export {
   setIngredients,
   fetchIngredientsFailed,
 } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrdersSuccess,
+  fetchOrdersStart,
+  fetchOrdersFail,
+} from "./order";
 export {
   auth,
   logout,
